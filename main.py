3f29ab5e8b3ea21a182daca95f7edfdf53e73c1f
kochemx <111306419+kochemx@users.noreply.github.com>
2022-08-15 23:22:13 +0300

    Added python script for list filterring

---
+++ main.py
@@ -0,0 +1,12 @@
+my_list = ['1', '12', '123', '1234', '12345', 'aa', 'bbbbb', 'c', '123123123', 'asfbwdbw', 'gwethwfvw']
+
+lenght_of_my_list = len(my_list)
+filtered_list = []
+for i in range(lenght_of_my_list):
+    if len(my_list[i]) <= 3:
+        filtered_list.append(my_list[i])
+
+print(my_list)
+print(filtered_list)
+
+
